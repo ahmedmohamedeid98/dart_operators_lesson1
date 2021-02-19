ae6820105a640d03c3a84d2484a0d7989a2d96d3
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-02-19 23:42:31 +0200

    add files

---
+++ android/app/src/main/kotlin/com/resocoder/lesson_1_operators/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.resocoder.lesson_1_operators
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
